c47d0b90f50efa156593b6ed652bfd22974c204e
Martin Juul <snowydane@users.noreply.github.com>
2018-03-07 15:14:26 +0100

    Create windows10-choco-setup.ps1

---
+++ windows10-choco-setup.ps1
@@ -0,0 +1,54 @@
+# Simple environment setup script
+
+# System Deps
+choco install vcredist-all
+
+# Install Applications
+choco install fiddler4
+choco install notepadplusplus
+choco install visualstudiocode
+choco install greenshot 
+choco install GoogleChrome
+choco install putty
+choco install skype
+choco install beyondcompare
+choco install spotify
+choco install winscp
+choco install wireshark
+choco install visualstudio2017enterprise
+choco install mobaxterm
+choco install filezilla
+choco install irfanview
+choco install keepassxc
+
+# Install Utilities
+choco install sysinternals
+choco install procmon 
+choco install windirstat 
+choco install msysgit
+choco install gitextensions
+choco install tortoisegit
+choco install 7zip
+choco install rdcman 
+#choco install TeraCopy 
+choco install logparser 
+#choco install tor-browser 
+choco install PowerShellGAC
+choco install procexp
+choco install cmder
+choco install autohotkey
+choco install wsus-offline-update
+
+# Dev
+choco install nodejs
+choco install npmtaskrunner
+choco install yarn
+choco install python2
+choco install python3
+
+# Internet Utilities
+choco install deluge # After install, configure for thin-client.
+choco install qbittorrent
+
+# Install Gaming stuff
+choco install steam
